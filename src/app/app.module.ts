c97b501557ec35c4b9cdd3951f9a4f065e07783c
Dominique <DCWTLT1@gmail.com>
2023-06-06 18:39:37 -0500

    created component for the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CityComponent } from './city/city.component';
 import { SeecityComponent } from './seecity/seecity.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CityComponent,
-    SeecityComponent
+    SeecityComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
