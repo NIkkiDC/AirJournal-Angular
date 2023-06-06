0b94c946bb2ee2c224ece0d4957546390505b878
Dominique <DCWTLT1@gmail.com>
2023-06-06 18:38:56 -0500

    created component for the display of the cities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CityComponent } from './city/city.component';
+import { SeecityComponent } from './seecity/seecity.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CityComponent
+    CityComponent,
+    SeecityComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/seecity/seecity.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seecity',
+  templateUrl: './seecity.component.html',
+  styleUrls: ['./seecity.component.css']
+})
+export class SeecityComponent {
+
+}
