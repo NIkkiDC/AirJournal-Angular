fede666bff6832fec8499bcf3fdaba9e677997dc
Dominique <DCWTLT1@gmail.com>
2023-06-07 11:24:34 -0500

    inserting a card on the home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PActivitiesComponent } from './p-activities/p-activities.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'p-activities', component: PActivitiesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
