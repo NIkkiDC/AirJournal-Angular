539aa3e9d902b498d2511a4c90b66efa626f0de4
Dominique <DCWTLT1@gmail.com>
2023-06-07 13:02:09 -0500

    the faqs has now been propery routed so now the faqs button works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PActivitiesComponent } from './p-activities/p-activities.component';
+import { FaqsComponent } from './faqs/faqs.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'p-activities', component: PActivitiesComponent },
+  { path: 'faqs', component: FaqsComponent },
 ];
 
 @NgModule({
