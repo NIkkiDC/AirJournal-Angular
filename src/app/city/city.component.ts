af0b0bb3e7624b0bf6ff5cb72b0761f188390f4b
Dominique <DCWTLT1@gmail.com>
2023-06-08 01:00:46 -0500

    attempting to illustrate the api to the user

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -1,29 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { PActivitiesComponent } from '../p-activities/p-activities.component';
 
 @Component({
   selector: 'app-city',
   templateUrl: './city.component.html',
   styleUrls: ['./city.component.css'],
 })
-export class CityComponent {
+export class CityComponent implements OnInit {
+  // cityId: number;
+  // activities: any[];
 
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private route:ActivatedRoute,
+    private http: HttpClient) { }
   
-  getAllCities(){
-    return this.http
-      .get(`http://localhost:8080/api/city`)
-      .subscribe((response) => {
-        console.log(response);
-      });
-  }
 
 ngOnInit(): void {
-    this.getAllCities();
-
+    this.route.paramMap.subscribe(params => {
+      // this.cityId = parseInt(params.get('id') || '') ;
+      this.loadActivities();
+    });
   }
 
-
-
+  loadActivities(): void {
+    this.http.get<any[]>(`http://localhost:8080/api/cities/${this.cityId}/activities`);
+        //   .subscribe(activities => { 
+        // this.activities = activities;
+        //       }, error => {
+      //   console.error('Failed to load activities:', error);
+      // });
+  }
 }
